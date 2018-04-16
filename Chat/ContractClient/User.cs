86cd3c9247e0109a875c2dff25af9d48b5465bf4
Maks <crazy13maks@gmail.com>
2018-04-16 20:01:29 +0300

    Start project

---
+++ Chat/ContractClient/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ContractClient
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+   public class User
+    {
+        public long Id { get; set; }
+        public String Login { get; set; }
+        public String Name { get; set; }
+        public int FriendsCount { get; set; }
+        public String Sity { get; set; }
+        public DateTime BirthDate { get; set; }
+        public byte[] Icon { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+    }
+}
